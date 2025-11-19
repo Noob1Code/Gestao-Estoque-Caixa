8ea2faac9d9a9a8b1c2a913265822c238f080d65
kayque <kayquefreitas08@gmail.com>
2025-11-18 23:39:44 -0300

    Squashed 'front/' content from commit 40e194cc
    git-subtree-dir: front
    git-subtree-split: 40e194ccf4344fa8865dc3ac494c1a6fd592ec8f

---
+++ .angular/cache/17.3.17/vite/deps/primeng_dropdown.js
@@ -0,0 +1,29 @@
+import {
+  DROPDOWN_VALUE_ACCESSOR,
+  Dropdown,
+  DropdownItem,
+  DropdownModule
+} from "./chunk-O6KGRB6S.js";
+import "./chunk-NXB5JTTQ.js";
+import "./chunk-YGHISWVM.js";
+import "./chunk-NTEXPIFX.js";
+import "./chunk-6IN5SLDW.js";
+import "./chunk-7NFPIS7I.js";
+import "./chunk-RF3O4RNU.js";
+import "./chunk-C6IC6WIT.js";
+import "./chunk-WVUL5KXD.js";
+import "./chunk-LCST64RR.js";
+import "./chunk-JZQAVOZS.js";
+import "./chunk-DJAXFXFY.js";
+import "./chunk-I54QP7DZ.js";
+import "./chunk-MW6N3XTI.js";
+import "./chunk-T76FZRMF.js";
+import "./chunk-6JJ7KVRE.js";
+import "./chunk-T4QU4GDF.js";
+export {
+  DROPDOWN_VALUE_ACCESSOR,
+  Dropdown,
+  DropdownItem,
+  DropdownModule
+};
+//# sourceMappingURL=primeng_dropdown.js.map
---
+++ src/app/core/models/produto.dto.ts
@@ -0,0 +1,26 @@
+export interface ProdutoResponseDTO {
+  id: number;
+  codigo: string;
+  nome: string;
+  categoria: string;
+  quantidadeEstoque: number;
+  precoUnitario: number;
+  ativo: boolean; 
+}
+export type TipoMovimento = 'ENTRADA' | 'AJUSTE';
+
+export interface ProdutoRequestDTO {
+  codigo: string;
+  nome: string;
+  categoria: string;
+  quantidadeEstoque: number;
+  precoUnitario: number;
+  ativo: boolean;
+}
+
+export interface MovimentoEstoqueRequestDTO {
+  tipo: TipoMovimento;
+  quantidade: number;
+  motivo?: string;
+  usuarioId: number;
+}
